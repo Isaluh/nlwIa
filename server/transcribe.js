e449361e31d6fac979ab7eba894017b353598213
Isaluh M <heloisamedeiros7@hotmail.com>
2023-09-17 02:29:53 -0300

    commit

---
+++ server/transcribe.js
@@ -0,0 +1,18 @@
+import { pipeline } from '@xenova/transformers';
+
+export async function transcribe(audio) {
+    try {
+        console.log("Realizando transcrição...")
+        const transcribe = await pipeline("automatic-speech-recognition", "Xenova/whisper-small");
+        const transcription = await transcribe(audio, {
+            chunk_length_s: 30,
+            stride_length_s: 5,
+            language: "portuguese",
+            task: "transcribe",
+        });
+        console.log("Transcrição relizada com sucesso");
+        return transcription?.text.replace("[Música]", "");
+    } catch (error) {
+        throw new Error(error);
+    }
+}
